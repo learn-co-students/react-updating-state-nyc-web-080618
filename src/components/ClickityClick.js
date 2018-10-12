abbca7867fc0fbf7a3a9225ccd456830d2f89f47
mccoleman75225 <michaelcoleman19@gmail.com>
2018-10-12 09:45:50 -0400

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,24 @@
 // Code ClickityClick Component Here
+import React from 'react';
+
+class ClickityClick extends React.Component{
+  constructor(){
+    super()
+    this.state = {clicked:false}
+  }
+
+  isClickedTag(){
+    debugger
+    if(this.state.clicked == false){
+      return "Button hasn't been clicked!"
+    } else {
+      return "Button has been clicked!"
+    }
+  }
+
+render(){
+  return(<button>{this.isClickedTag()}</button>)
+}
+}
+
+export default ClickityClick;
